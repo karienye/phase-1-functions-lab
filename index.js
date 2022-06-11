8fafa9ac50ce6a6400009a4ef022ccaff78ee93a
leah karienye <karienyeleah@gmail.com>
2022-06-11 18:12:52 +0300

    Done.

--- index.js
+++ index.js
@@ -1,28 +1,47 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks(someValue) {
-    return (someValue - '42')
+  if (someValue>42){
+    return someValue - 42;
+  }
+  else if (someValue<42){
+    return 42 - someValue;
+  }
+  else {
+    return 42; 
+  }
+    
     //returns the number of blocks given a value
   }
 
   function distanceFromHqInFeet(someValue) {
-    return ((someValue - '42') * 264)
+    return distanceFromHqInBlocks(someValue) * 264;
     //returns the number of blocks given a value
   }
   function distanceTravelledInFeet(start, destination) {
-      return ((destination - start) * 264)
+    if (start<destination){
+      return (destination - start) * 264;
+    }
+      else{
+        return (start - destination) * 264;
+      }
     //returns the number of feet traveled
   }
   function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination)
+
     //returns the fare for the customer
-    if (distanceTravelledInFeet > '2500'){
-     return 'cannot travel that far'
+    if (distance<400){
+      return 0;
     }
-    else if (distanceTravelledInFeet <= '400'){
-    return 0;
+    else if (distance > 400 && distance<=2000){
+     return (distance - 400) * 0.02;
+    }
+    else if (distance > 2000 && distance<2500){
+    return 25;
     }
   
-  else if ((distanceTravelledInFeet > 400 ) && (distanceTravelledInFeet< 2000)){
-  return ((distanceTravelledInFeet)- 400) * 2 ;
+  else {
+  return  'cannot travel that far' ;
   }
 
   }
